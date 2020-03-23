2b13cae592d614fd7b3c25e26f8a8701fae04df7
comradeDing <dingfaching@gmail.com>
2020-03-23 13:08:33 -0500

    Update package. Comment environment file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+/*
+  * Used when developing "local" configuration
+  *
+*/
 
 export const environment = {
   modelUrl: 'http://localhost:50001/models/',
