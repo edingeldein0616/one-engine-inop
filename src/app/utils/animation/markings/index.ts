4238cb3f142bc5d9832a37167eb7cee3ecf51f64
unknown <edingeldein0616@gmail.com>
2021-01-28 23:00:11 -0600

    Index ZST markings in animaiton.

--- src/app/utils/animation/markings/index.ts
+++ src/app/utils/animation/markings/index.ts
@@ -1,5 +1,8 @@
+import { fromEventPattern } from 'rxjs';
+
 export * from './accelerated';
 export * from './markings';
 export * from './pfactor';
 export * from './slipstream';
 export * from './torque';
+export * from './zerosideslip';
